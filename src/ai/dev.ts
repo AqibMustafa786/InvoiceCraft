4770044d6a50c1e1e4ff63b99d676745442ac890
AQIB Mustafa <aqib2k1@gmail.com>
2026-01-04 17:18:56 +0000

    restore last version

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-// Flows will be imported for their side effects in this file.
-import './flows/send-document-flow';
-import './flows/generate-pdf-flow';
-import './flows/send-password-reset-flow';
-import './flows/stripe-checkout-flow';
-
-    
