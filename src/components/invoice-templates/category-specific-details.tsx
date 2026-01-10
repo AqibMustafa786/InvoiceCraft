46187b83451b346e15c0b618f9f82943c8725462
AQIB Mustafa <aqib2k1@gmail.com>
2026-01-10 02:10:33 +0000

    not working E-commerce Details heading nd fields in live preview

--- src/components/invoice-templates/category-specific-details.tsx
+++ src/components/invoice-templates/category-specific-details.tsx
@@ -52,7 +52,7 @@ export const CategorySpecificDetails: FC<{ invoice: Invoice, t: any }> = ({ invo
 
     // A helper function to render the category details and the custom fields without a heading
     const renderDetailsWithCustomFields = (DetailsComponent: React.FC<any> | null) => {
-      const hasCategoryData = invoice[category.split(" ")[0].toLowerCase() as keyof Invoice];
+      const hasCategoryData = invoice[(category.split(" ")[0].toLowerCase() as keyof Invoice)];
       return (
         <>
             {DetailsComponent && hasCategoryData ? <DetailsComponent {...commonProps} /> : null}
